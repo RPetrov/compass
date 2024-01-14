af256909ae6941c2ead39f56cfa6c8d094841c67
Your Name <you@example.com>
2024-01-14 21:37:30 +0300

    init commit

---
+++ app/src/main/java/rpetrov/compass/MainActivity.kt
@@ -0,0 +1,19 @@
+package rpetrov.compass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Вью. Image, Text, languages, tablet
+ * Сенсоры (https://developer.android.com/develop/sensors-and-location/sensors/sensors_overview)
+ * onResume / on Start, dialog
+ * Анимация
+ * Шеринг
+ * Denug Tools
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
